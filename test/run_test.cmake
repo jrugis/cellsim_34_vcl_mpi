dd72c6a574f9735bd15dc8496938403e841f1ddf
Chris Scott <chris.scott@nesi.org.nz>
2017-09-01 12:55:40 +1200

    Updating tests for MPI version

--- test/run_test.cmake
+++ test/run_test.cmake
@@ -11,6 +11,10 @@ message(STATUS "  Python reduce: ${TEST_PY_REDUCE}")
 message(STATUS "  Python found: ${PYTHONINTERP_FOUND}")
 message(STATUS "  Python executable: ${PYTHON_EXECUTABLE}")
 message(STATUS "  Enable gprof: ${ENABLE_GPROF}")
+message(STATUS "  MPIEXEC: ${MPIEXEC}")
+message(STATUS "  MPIEXEC_NUMPROC_FLAG: ${MPIEXEC_NUMPROC_FLAG}")
+message(STATUS "  MPIEXEC_PREFLAGS: ${MPIEXEC_PREFLAGS}")
+message(STATUS "  MPIEXEC_POSTFLAGS: ${MPIEXEC_POSTFLAGS}")
 if (ENABLE_GPROF)
     message(STATUS "    gprof: ${GPROF_PROGRAM}")
     message(STATUS "    gprof2dot: ${GPROF2DOT_PROGRAM}")
@@ -24,45 +28,51 @@ execute_process(COMMAND ${CMAKE_COMMAND} -E remove_directory ${TEST_RUN_DIR})
 execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${TEST_RUN_DIR})
 
 #
-# copy mesh file
+# copy mesh files
 #
-file(COPY ${TEST_SRC_DIR}/cell01m_HARMONIC_100p.msh DESTINATION ${TEST_RUN_DIR})
-file(RENAME ${TEST_RUN_DIR}/cell01m_HARMONIC_100p.msh ${TEST_RUN_DIR}/cs.msh)
+foreach (index RANGE 1 7)
+    file(COPY ${TEST_SRC_DIR}/out_N4_p3-p2-p4-${index}tet.msh DESTINATION ${TEST_RUN_DIR})
+    file(RENAME ${TEST_RUN_DIR}/out_N4_p3-p2-p4-${index}tet.msh ${TEST_RUN_DIR}/cs${index}.msh)
+endforeach()
 
 #
 # copy parameter file
 #
-string(REGEX MATCH "03" IS_3VAR ${TEST_BINARY})
-if (IS_3VAR)
-    file(COPY ${TEST_SRC_DIR}/generic3d_03-cs.dat DESTINATION ${TEST_RUN_DIR})
-    file(RENAME ${TEST_RUN_DIR}/generic3d_03-cs.dat ${TEST_RUN_DIR}/cs.dat)
-else()
-    file(COPY ${TEST_SRC_DIR}/generic3d_04-cs.dat DESTINATION ${TEST_RUN_DIR})
-    file(RENAME ${TEST_RUN_DIR}/generic3d_04-cs.dat ${TEST_RUN_DIR}/cs.dat)
-endif()
+foreach (index RANGE 1 7)
+    string(REGEX MATCH "03" IS_3VAR ${TEST_BINARY})
+    if (IS_3VAR)
+        file(COPY ${TEST_SRC_DIR}/generic3d_03-cs.dat DESTINATION ${TEST_RUN_DIR})
+        file(RENAME ${TEST_RUN_DIR}/generic3d_03-cs.dat ${TEST_RUN_DIR}/cs${index}.dat)
+    else()
+        file(COPY ${TEST_SRC_DIR}/generic3d_04-cs.dat DESTINATION ${TEST_RUN_DIR})
+        file(RENAME ${TEST_RUN_DIR}/generic3d_04-cs.dat ${TEST_RUN_DIR}/cs${index}.dat)
+    endif()
 
-#
-# If testing python reduce, disable C++ reduce
-#
-if (TEST_PY_REDUCE)
-    # change variable in parameter file
-    execute_process(
-        COMMAND ${CMAKE_COMMAND} -E chdir ${TEST_RUN_DIR}
-        sed -e "s/0.05 120 1/0.05 120 0/g" cs.dat
-        RESULT_VARIABLE STATUS
-        OUTPUT_FILE ${TEST_RUN_DIR}/cs.dat.new
-    )
-    if (STATUS)
-        message(FATAL_ERROR "sed command failed: ${STATUS}")
-    endif ()
-    file(RENAME ${TEST_RUN_DIR}/cs.dat.new ${TEST_RUN_DIR}/cs.dat)
-endif()
+    #
+    # If testing python reduce, disable C++ reduce
+    #
+    if (TEST_PY_REDUCE)
+        # change variable in parameter file
+        execute_process(
+            COMMAND ${CMAKE_COMMAND} -E chdir ${TEST_RUN_DIR}
+            sed -e "s/0.05 120 1/0.05 120 0/g" cs${index}.dat
+            RESULT_VARIABLE STATUS
+            OUTPUT_FILE ${TEST_RUN_DIR}/cs${index}.dat.new
+        )
+        if (STATUS)
+            message(FATAL_ERROR "sed command failed: ${STATUS}")
+        endif ()
+        file(RENAME ${TEST_RUN_DIR}/cs${index}.dat.new ${TEST_RUN_DIR}/cs${index}.dat)
+    endif()
+endforeach()
 
 #
 # run cellsim
 #
 execute_process(
-    COMMAND ${CMAKE_COMMAND} -E chdir ${TEST_RUN_DIR} ${TEST_BINARY}
+    COMMAND ${CMAKE_COMMAND} -E chdir ${TEST_RUN_DIR}
+            ${MPIEXEC} ${MPIEXEC_NUMPROC_FLAG} 7
+            ${MPIEXEC_PREFLAGS} ${TEST_BINARY} ${MPIEXEC_POSTFLAGS}
     RESULT_VARIABLE STATUS
 )
 if (STATUS)
@@ -147,7 +157,7 @@ endif()
 if (NOT SKIP_COMPARE)
     execute_process(
         COMMAND ${CMAKE_COMMAND} -E chdir ${TEST_RUN_DIR}
-        ${PYTHON_EXECUTABLE} ${TEST_SRC_DIR}/cs_compare_peaks.py cR.bin ${TEST_SRC_DIR}/generic3d-cR.bin
+        ${PYTHON_EXECUTABLE} ${TEST_SRC_DIR}/cs_compare_peaks.py cR1.bin ${TEST_SRC_DIR}/generic3d-cR.bin
         RESULT_VARIABLE STATUS
     )
     if (STATUS)
